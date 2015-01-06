7b36e8953e84f0fdb90556d1f034143c04f0bdb5
Robin Willis <robincwillis@gmail.com>
2015-01-06 10:46:51 -0500

    making handsontable global

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'robincwillis:handsontable',
   summary: 'Handsontable, forked to use bootstrap date picker',
-  version: '0.12.4',
+  version: '0.12.5',
   git: 'https://github.com/robincwillis/meteor-handsontable.git'
 });
 
